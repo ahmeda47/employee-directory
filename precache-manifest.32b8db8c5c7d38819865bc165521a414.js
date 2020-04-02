88f1dab300aaed203d286c5bc728212d48856539
Abdiwahab Ahmed <abdiwahabahmed@Abdiwahabs-MacBook-Air.local>
2020-04-02 16:08:18 -0400

    Updates

---
+++ precache-manifest.32b8db8c5c7d38819865bc165521a414.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccba862e98117a41c697f07ff2d88c6d",
+    "url": "/employee-directory/index.html"
+  },
+  {
+    "revision": "9a05accec779b28ba26f",
+    "url": "/employee-directory/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d6dbafe518692f4b4cae",
+    "url": "/employee-directory/static/js/2.2e554377.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/employee-directory/static/js/2.2e554377.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a05accec779b28ba26f",
+    "url": "/employee-directory/static/js/main.7ddd287f.chunk.js"
+  },
+  {
+    "revision": "1555ac21ead60afe404d",
+    "url": "/employee-directory/static/js/runtime-main.2264778b.js"
+  }
+]);
